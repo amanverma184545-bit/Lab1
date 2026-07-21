2c3e7ec9ddfa11eaff45c27561883c56ff1268e9
Aman Verma <amanverma184545@gmail.com>
2026-07-21 14:49:06 +0530

    add fuction

---
+++ hello.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+    console.log(`Welcome to ${name}`);
+}
+const sayHi = (name) => {
+        console.log(`Welcome to ${name}`);
+};
+function sum(a,b){
+    return a+b;
+}
+
+sayHello("Aman Verma");
+console.log(`sum of 2 and 5 is ${sum(2,5)}`);
