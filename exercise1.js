24cb9284df0d0ed241f0bcc263a3ef4c39b3b705
Aman Verma <amanverma184545@gmail.com>
2026-07-22 11:23:13 +0530

    Number to words

---
+++ exercise1.js
@@ -0,0 +1,30 @@
+const toWords = (digit)=>{
+    switch(digit){
+        case 0:
+            return "Zero";
+        case 1:
+            return "One";
+        case 2:
+            return"Two";
+        case 3:
+            return "Three";
+        case 4:
+            return"Four";
+        case 5:
+            return"Five";
+        case 6:
+            return "Six"; 
+        case 7:
+            return "Seven";
+        case 8:
+            return"Eight" ;
+        case 9:
+            return"Nine";
+            default:
+                return "Invalid Terminal";
+
+    }
+};
+    console.log(toWords(5));
+    console.log(toWords(3));
+    console.log(toWords(7));
